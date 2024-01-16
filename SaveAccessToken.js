3ac9f0cf0e369da4703448d78779172e5474d917
Jim Madden <jim@readme.io>
2024-01-16 14:59:19 -0800

    logic fix

--- SaveAccessToken.js
+++ SaveAccessToken.js
@@ -30,10 +30,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
       // Function to save the access_token value into local storage
       function saveAccessTokenToLocalStorage(accessToken) {
+        // Check if the string starts and ends with double quotes
+        let token;
+        if (accessToken.startsWith('"') && accessToken.endsWith('"')) {
+          // Remove double quotes using slice
+          token = accessToken.slice(1, -1);
+          console.log(token);
+        } else {
+          // Do something else if the string doesn't have quotes at both ends
+          console.log('String does not have quotes at both ends.');
+        }
         if (accessToken) {
           const storedAccessToken = localStorage.getItem('access_token');
           if (!storedAccessToken || storedAccessToken !== accessToken) {
-            localStorage.setItem('access_token', accessToken);
+            localStorage.setItem('access_token', token);
             console.log('Access token saved in local storage:');
           }
         }
@@ -74,7 +84,7 @@ document.addEventListener('DOMContentLoaded', function () {
             tryItButton.addEventListener('click', function (event) {
               event.preventDefault();
               // Check if the current URL ends with '/token'
-              if (window.location.pathname.endsWith('/token')) {
+              if (window.location.pathname.endsWith('/token-1')) {
                 // Execute the additional logic after a delay on the /token page
                 additionalLogicAfterDelay();
               }
--- UseAccessToken.js
+++ UseAccessToken.js
@@ -1,47 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
   let isUpdated = false;
 
-  function updateInputWithLocalStorageToken() {
-    var inputField = document.getElementById('APIAuth-token');
-    var inputValue = inputField.value;
+  const updateBearerToken = () => {
+    setTimeout(() => {
+      let inputField = document.getElementById('APIAuth-token');
+      let inputValue = inputField?.value;
 
-    // Check if access_token exists in local storage
-    var storedToken = localStorage.getItem('access_token');
+      // Check if access_token exists in local storage
+      let storedToken = localStorage.getItem('access_token');
 
-    if (!storedToken) {
-      console.log('Access token does not exist in local storage.');
-      return; // Stop the code execution if access_token does not exist
-    }
-
-    if (inputValue !== storedToken || inputValue === '') {
-      // Update the input field value with the access_token from local storage
-      inputField.value = storedToken;
-
-      inputField.addEventListener('input');
-
-      console.log('Input field updated with local storage value:', storedToken);
-      isUpdated = true;
-      // You may want to update the local storage here if needed
-      const event = new Event('input', { bubbles: true, cancelable: true });
-      inputField.dispatchEvent(event);
-    } else if (!isUpdated) {
-      console.log('Input field value is the same as the local storage value.');
-    }
-  }
-
-  function checkAndUpdateOnMutation(mutationsList, observer) {
-    for (var mutation of mutationsList) {
-      if (mutation.type === 'childList' || mutation.type === 'attributes') {
-        if (!window.location.href.endsWith('/token')) {
-          isUpdated = false;
-          setTimeout(updateInputWithLocalStorageToken, 1000); // Delay of 1 second
-        }
+      if (!storedToken) {
+        console.log('Access token does not exist in local storage.');
+        return; // Stop the code execution if access_token does not exist
       }
-    }
-  }
+
+      const event = new Event('change', { bubbles: true });
+
+      if (inputValue !== storedToken || inputValue === '') {
+        let nativeInputValueSetter = Object.getOwnPropertyDescriptor(
+          window.HTMLInputElement.prototype,
+          'value'
+        ).set;
+        nativeInputValueSetter.call(inputField, storedToken);
+
+        // Dispatch 'input' event to mimic the user input
+        const event = new Event('input', { bubbles: true, cancelable: true });
+        inputField.dispatchEvent(event);
+
+        console.log('Input field updated with local storage access token');
+        isUpdated = true;
+      } else if (!isUpdated) {
+        inputField.dispatchEvent(event);
+
+        console.log(
+          'Input field value is the same as the local storage value.'
+        );
+      }
+    }, 1000);
+  };
 
   // Create a MutationObserver instance
-  var observer = new MutationObserver(checkAndUpdateOnMutation);
+  var observer = new MutationObserver(updateBearerToken);
 
   // Observe changes in the document's body and its descendants
   observer.observe(document.body, {
@@ -52,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Call the function on initial load with a delay, but not on "/token" pages
   if (!window.location.href.endsWith('/token-1')) {
-    setTimeout(updateInputWithLocalStorageToken, 1000); // Delay of 1 second
+    updateBearerToken();
+    window.onpopstate = updateBearerToken;
   }
 });
